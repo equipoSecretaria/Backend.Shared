0032d241c4839318c4d86b776c9d7c3635f547e3
maomauricio1983 <maomauriciomunoz@gmail.com>
2022-05-18 16:47:42 -0500

    se agregan clases Cementerio.cs  y Funeraria.cs

---
+++ Backend.Shared.Entities/Models/Tramites/Cementerio.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Backend.Shared.Entities.Models.Tramites
+{
+    public partial class Cementerio
+    {
+        public int NROIDENT { get; set; }
+        public string TIPO_I { get; set; }
+        public string RAZON_S { get; set; }
+        public string DIRECCION { get; set; }
+        public string TELEFONO_1 { get; set; }
+        public string TELEFONO_2 { get; set; }
+        public string TIPO_I_REP { get; set; }
+        public string NROIDENT_REP { get; set; }
+        public string NOMBRE_REP { get; set; }
+        public string CREMACION { get; set; }
+    }
+}
---
+++ Backend.Shared.Entities/Models/Tramites/Funeraria.cs
@@ -0,0 +1,19 @@
+﻿namespace Backend.Shared.Entities.Models.Tramites
+{
+    public partial class Funeraria
+    {
+        public string TIPO_I { get; set; }
+        public int NROIDENT { get; set; }
+        public string RAZON_S { get; set; }
+        public string DIRECCION { get; set; }
+        public string TELEFONO_1 { get; set; }
+        public string TIPO_I_PROP { get; set; }
+        public int NROIDENT_PROP { get; set; }
+        public string NOMBRE_PROP { get; set; }
+        public int NUM_SALAS { get; set; }
+        public string TIPO_I_REP { get; set; }
+        public int NROIDENT_REP { get; set; }
+        public string NOMBRE_REP { get; set; }
+        public string TELEFONO_2 { get; set; }
+    }
+}
